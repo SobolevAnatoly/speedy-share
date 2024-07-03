64fa425e8138fb433f97d0c8e5797a8615a0fb3a
Anatolii S <Sobolev.Anatoly@gmail.com>
2024-07-03 19:09:42 +0000

    wp scaffold plugin speedy-share

---
+++ speedy-share.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Speedy Share
+ * Plugin URI:      https://github.com/SobolevAnatoly/speedy-share
+ * Description:     Test task for Web4Pro company. WordPress plugin that add a shortcode what can be placed to any page to display the form for adding a CV on the site.
+ * Author:          Anatolii S.
+ * Author URI:      https://github.com/SobolevAnatoly
+ * Text Domain:     speedy-share
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Speedy_Share
+ */
+
+// Your code starts here.
