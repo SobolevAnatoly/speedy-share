9d99f381f0680bda1b048e65b3c2007eff8b9c5c
Anatolii S <Sobolev.Anatoly@gmail.com>
2024-07-03 20:18:58 +0000

    Set min PHP & WP ver. Add License GPL v2

--- speedy-share.php
+++ speedy-share.php
@@ -1,13 +1,18 @@
 <?php
+
 /**
- * Plugin Name:     Speedy Share
- * Plugin URI:      https://github.com/SobolevAnatoly/speedy-share
- * Description:     Test task for Web4Pro company. WordPress plugin that add a shortcode what can be placed to any page to display the form for adding a CV on the site.
- * Author:          Anatolii S.
- * Author URI:      https://github.com/SobolevAnatoly
- * Text Domain:     speedy-share
- * Domain Path:     /languages
- * Version:         0.1.0
+ * Plugin Name:       Speedy Share
+ * Plugin URI:        https://github.com/SobolevAnatoly/speedy-share
+ * Description:       Test task for Web4Pro company. WordPress plugin that add a shortcode what can be placed to any page to display the form for adding a CV on the site.
+ * Author:            Anatolii S.
+ * Author URI:        https://github.com/SobolevAnatoly
+ * Text Domain:       speedy-share
+ * Domain Path:       /languages
+ * Version:           0.1.0
+ * Requires at least: 6.4.1
+ * Requires PHP:      7.4
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package         Speedy_Share
  */
